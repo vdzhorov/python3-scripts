e6e9d1477a1f79b051fab808bcd966da06bedcf2
Valentin Dzhorov <dzhorov@delta.bg>
2020-01-30 14:18:21 +0200

    Added scripts for importing the base class and using the set and remove definitions

---
+++ icinga2-downtime-handler/icinga2/set_host_downtime.py
@@ -0,0 +1,11 @@
+from downtime_handler import Handler
+import sensitive_data
+from sys import argv
+
+script, host = argv
+
+hostname=sensitive_data.hostname
+username=sensitive_data.username
+password=sensitive_data.password
+
+Handler(hostname, username, password).set_host_downtime(host)
